dceaac1055566189252c1603b2f53a50084e070b
Luca Candela <ilcande_prime@live.it>
2024-08-26 21:19:39 +0200

    add cart context

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 import HomePage from './components/pages/HomePage';
 import Products from './components/products/Products';
 import ProductDetail from './components/products/ProductDetail';
+import { CartProvider } from './context/CartContext.js';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:productId" element={<ProductDetail />} />
-      </Routes>
-    </Router>
+    <CartProvider>
+      <Router>
+        <Navbar />
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:productId" element={<ProductDetail />} />
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 }
 
